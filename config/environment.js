db8caa671e7583f9ddc3bbf888191420db446592
Uzzal Kar <uzzalkar12@gmail.com>
2023-09-06 15:02:41 +0600

    role moule

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ export const environment = "dev";
 const apiDev = "http://localhost:8000";
 const apiProd = "http://18.119.12.43";
 
-const localDev = "http://localhost:3000/";
+const localDev = "http://localhost:3002/";
 const localProd = "http://3.134.82.149/";
 
 export const baseURL = environment === "dev" ? localDev : localProd;
