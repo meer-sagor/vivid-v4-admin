d1719eecf7ef06b7e9d6b59bf97e4ea26e29418a
Uzzal Kar <uzzalkar12@gmail.com>
2023-09-06 10:48:50 +0600

    envrioment

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 export const environment = "dev";
-const adminURL = "http://localhost:3002/";
+const adminURL = "http://localhost:3000/";
 
 export const baseURL = environment === "dev" ? adminURL : adminURL;
 export const apiURL = environment === "dev" ? "http://localhost:8000" : "http://localhost:8000";
