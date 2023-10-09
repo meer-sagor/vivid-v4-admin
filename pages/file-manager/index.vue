26adf2bc1a69d431e4e63346185cfd58053d2cdb
Uzzal Kar <uzzalkar12@gmail.com>
2023-10-09 23:02:40 +0600

    image module testing

--- pages/file-manager/index.vue
+++ pages/file-manager/index.vue
@@ -1,9 +1,55 @@
 <template>
   <div class="grid p-fluid">
     <div class="col-12">
-      <div class="card"></div>
+      <div class="card">
+<!--        <template v-if="fetching">-->
+<!--          <file-manager v-bind:settings="settings"></file-manager>-->
+<!--          <file-manager v-bind:settings="medias"></file-manager>-->
+<!--        </template>-->
+<!--        <div class="flex justify-content-center">-->
+<!--          <ProgressSpinner v-if="spinner" />-->
+<!--        </div>-->
+      </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script setup>
+import {ref, onMounted, nextTick} from "vue";
+import { useMediaStore } from "@/stores/useMediaStore";
+// import FileManager from 'laravel-file-manager'
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
+const mediaStore = useMediaStore();
+const medias = ref([]);
+const fetching = ref(true);
+const spinner = ref(true);
+const loading = ref(true);
+
+const fetchMedias = async () => {
+  spinner.value = true;
+  await mediaStore.getMedias();
+  spinner.value = false;
+  if (mediaStore.medias) {
+    fetching.value = true;
+    medias.value = mediaStore.medias;
+  }
+};
+
+const settings = computed(() => {
+  return {
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${window.localStorage.getItem('access_token')}`,
+    },
+    baseUrl: 'http://127.0.0.1:8000/api/images',
+  };
+})
+
+onMounted(async () => {
+  await nextTick();
+  await fetchMedias();
+});
+
+</script>
+
