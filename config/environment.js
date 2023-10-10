ffb9b1462fdf1487c229761ed37b98bb1e40a9da
hmnredoy <hmnredoy@gmail.com>
2023-10-10 23:43:37 +0600

    update with develop4

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-export const environment = "dev";
+export const environment = "prod";
 const apiDev = "http://localhost:8000";
 const apiProd = "http://18.217.130.231";
 
@@ -7,4 +7,3 @@ const localProd = "http://18.116.100.219/";
 
 export const baseURL = environment === "dev" ? localDev : localProd;
 export const apiURL = environment === "dev" ? apiDev : apiProd;
-
